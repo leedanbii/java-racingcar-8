c56222f75553aab7435bc932a62d79dea96de7be
leedanbii <eksqlgodqhr@naver.com>
2025-10-24 22:38:43 +0900

    feat: 자동차 경주 기능 구현

---
+++ src/main/java/racingcar/domain/RaceService.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RaceService {
+    private static final String RACE_PROGRESS_SEPARATOR = " : ";
+    private static final String RACE_STEP_MARK = "-";
+
+    public void runRace(List<Car> cars, int attempts) {
+        for (int i = 0; i < attempts; i++) {
+            moveCars(cars);
+            printRace(cars);
+        }
+    }
+
+    public void moveCars(List<Car> cars) {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            car.move(randomNumber);
+        }
+    }
+
+    public void printRace(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + RACE_PROGRESS_SEPARATOR);
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print(RACE_STEP_MARK);
+            }
+            System.out.println();
+        }
+    }
+}
