595ad351a8ff9b6ddb2e00aa6e7eeed608822618
leedanbii <eksqlgodqhr@naver.com>
2025-10-25 20:50:25 +0900

    refactor(RaceService): 우승자 선정 기능 추가 및 접근 제어자 수정

--- src/main/java/racingcar/domain/RaceService.java
+++ src/main/java/racingcar/domain/RaceService.java
@@ -7,21 +7,22 @@ public class RaceService {
     private static final String RACE_PROGRESS_SEPARATOR = " : ";
     private static final String RACE_STEP_MARK = "-";
 
-    public void runRace(List<Car> cars, int tryCount) {
+    public List<Car> runRace(List<Car> cars, int tryCount) {
         for (int i = 0; i < tryCount; i++) {
             moveCars(cars);
             printRace(cars);
         }
+        return findWinners(cars);
     }
 
-    public void moveCars(List<Car> cars) {
+    private void moveCars(List<Car> cars) {
         for (Car car : cars) {
             int randomNumber = Randoms.pickNumberInRange(0, 9);
             car.move(randomNumber);
         }
     }
 
-    public void printRace(List<Car> cars) {
+    private void printRace(List<Car> cars) {
         for (Car car : cars) {
             System.out.print(car.getName() + RACE_PROGRESS_SEPARATOR);
             for (int i = 0; i < car.getPosition(); i++) {
@@ -29,5 +30,18 @@ public class RaceService {
             }
             System.out.println();
         }
+    }
+
+    private List<Car> findWinners(List<Car> cars) {
+        if (cars == null || cars.isEmpty()) {
+            return List.of();
+        }
+        int maxPosition = cars
+                .stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream().filter(car -> car.getPosition() == maxPosition).toList();
     }
 }
