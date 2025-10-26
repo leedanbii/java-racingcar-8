50f43e99ac0b7ed8e047ad68ead54fa8b923a321
leedanbii <eksqlgodqhr@naver.com>
2025-10-26 23:37:16 +0900

    test: RaceControllerTest 테스트 코드 추가

---
+++ src/test/java/racingcar/controller/RaceControllerTest.java
@@ -0,0 +1,121 @@
+package racingcar.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.Assertions;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RaceService;
+import racingcar.dto.UserInput;
+import racingcar.formatter.ResultFormatter;
+import racingcar.view.Input;
+import racingcar.view.Output;
+
+public class RaceControllerTest {
+
+    @Test
+    void startRace_shouldPrintWinnersCorrectly() {
+        // given
+        Input testInput = new Input() {
+            @Override
+            public UserInput readUserInput() {
+                return new UserInput("pobi,woni", "1");
+            }
+        };
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Output testOutput = new Output() {
+            @Override
+            public void printWinners(String winners) {
+                System.out.print(winners);
+            }
+        };
+        System.setOut(new PrintStream(out));
+
+        RaceController controller = new RaceController(
+                testInput,
+                testOutput,
+                new RaceService(),
+                new ResultFormatter()
+        );
+
+        // when
+        controller.startRace();
+
+        // then
+        String output = out.toString();
+        assertThat(output).contains("pobi", "woni");
+    }
+
+    @Test
+    void startRace_shouldRunRaceCorrectly_whenMultipleTryCount() {
+        // given
+        Input testInput = new Input() {
+            @Override
+            public UserInput readUserInput() {
+                return new UserInput("pobi,jun,  woni", "4");
+            }
+        };
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Output testOutput = new Output() {
+            @Override
+            public void printWinners(String winners) {
+                System.out.print(winners);
+            }
+        };
+        System.setOut(new PrintStream(out));
+
+        RaceController controller = new RaceController(
+                testInput,
+                testOutput,
+                new RaceService(),
+                new ResultFormatter()
+        );
+
+        // when
+        controller.startRace();
+
+        // then
+        String output = out.toString();
+        assertThat(output).isNotEmpty();
+        assertThat(output).contains("pobi", "jun", "woni");
+    }
+
+    @Test
+    void startRace_assertRandomNumberInRangeExample() {
+        // given
+        Input testInput = new Input() {
+            @Override
+            public UserInput readUserInput() {
+                return new UserInput("pobi,woni", "1");
+            }
+        };
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Output testOutput = new Output() {
+            @Override
+            public void printWinners(String winners) {
+                System.out.print(winners);
+            }
+        };
+        System.setOut(new PrintStream(out));
+
+        RaceController controller = new RaceController(
+                testInput,
+                testOutput,
+                new RaceService(),
+                new ResultFormatter()
+        );
+
+        // when
+        Assertions.assertRandomNumberInRangeTest(
+                () -> controller.startRace(),
+                4, 3  // 4는 이동, 3은 정지
+        );
+
+        // then
+        String output = out.toString();
+        assertThat(output).contains("pobi", "woni");
+    }
+}
