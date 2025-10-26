725998a3e43925738ca022102242abc3ee96147c
leedanbii <eksqlgodqhr@naver.com>
2025-10-27 00:32:55 +0900

    feat: main() 작성하여 프로그램 실행 흐름 연결

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.domain.RaceService;
+import racingcar.formatter.ResultFormatter;
+import racingcar.view.Input;
+import racingcar.view.Output;
+import racingcar.view.RaceView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceService raceService = new RaceService();
+        Input input = new Input();
+        Output output = new Output();
+        ResultFormatter formatter = new ResultFormatter();
+
+        RaceController controller = new RaceController(input, output, raceService, formatter);
+        RaceView view = new RaceView(controller);
+
+        try {
+            view.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 }
