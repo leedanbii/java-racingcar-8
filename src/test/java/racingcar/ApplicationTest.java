ef951aac9485d6c17e41abc1d3082810c71e718c
leedanbii <eksqlgodqhr@naver.com>
2025-10-27 00:36:56 +0900

    test: ApplicationTest 테스트 메서드 수정 및 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -6,29 +6,91 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.validator.UserInputValidator.BLANK_CAR_NAME;
+import static racingcar.validator.UserInputValidator.DUPLICATE_CAR_NAME;
+import static racingcar.validator.UserInputValidator.EMPTY_CAR_NAME_LIST;
+import static racingcar.validator.UserInputValidator.EMPTY_TRY_COUNT;
+import static racingcar.validator.UserInputValidator.LESS_THAN_ONE_TRY_COUNT;
+import static racingcar.validator.UserInputValidator.NOT_NUMBER_TRY_COUNT;
+import static racingcar.validator.UserInputValidator.TOO_LONG_CAR_NAME;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
     @Test
-    void 기능_테스트() {
+    void application_runsRaceSuccessfully() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void application_shouldThrowException_whenEmptyCarNames() {
+        assertSimpleTest(() -> {
+            runException("", "2");
+            assertThat(output()).contains(EMPTY_CAR_NAME_LIST);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenBlankCarName() {
+        assertSimpleTest(() -> {
+            runException("jin,   ", "2");
+            assertThat(output()).contains(BLANK_CAR_NAME);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenTryCountIsInvalid() {
+        assertSimpleTest(() -> {
+            runException("pobi, jieun ", "0");
+            assertThat(output()).contains(LESS_THAN_ONE_TRY_COUNT);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenCarNameTooLong() {
+        assertSimpleTest(() -> {
+            runException("pobi,pobiiiii", "4");
+            assertThat(output()).contains(TOO_LONG_CAR_NAME);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenDuplicateCarName() {
+        assertSimpleTest(() -> {
+            runException("pobi,pobi", "4");
+            assertThat(output()).contains(DUPLICATE_CAR_NAME);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenEmptyTryCount() {
+        assertSimpleTest(() -> {
+            runException("pobi,jin", "  ");
+            assertThat(output()).contains(EMPTY_TRY_COUNT);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenNotNumberTryCount() {
+        assertSimpleTest(() -> {
+            runException("pobi, jin", "*");
+            assertThat(output()).contains(NOT_NUMBER_TRY_COUNT);
+        });
+    }
+
+    @Test
+    void application_shouldThrowException_whenLessThanOneTryCount() {
+        assertSimpleTest(() -> {
+            runException("pobi, jin", "0");
+            assertThat(output()).contains(LESS_THAN_ONE_TRY_COUNT);
+        });
     }
 
     @Override
